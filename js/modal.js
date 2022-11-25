62adeb857be10c0b09d0be0a9651dcfe4a44a04e
Paulo César de Sousa Morais <paulumora@hotmail.com>
2022-11-25 18:55:32 -0300

    Project-Ofic

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+
+var modal = document.getElementById("myModal"); /* Pega o modal*/
+
+/* Pega o botão que abre o modal*/
+var btn = document.getElementById("myBtn");
+
+/* Pega o elemento <span> que fecha o modal*/
+var span = document.getElementsByClassName("close")[0];
+
+/* Quando o usuário clicar no botão, abre o modal*/
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+/*  Quando o usuário clicar em <span> (x), fecha o modal*/
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+/*  Quando o usuário clicar em qualquer lugar fora do modal, feche-o*/
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
--- js/validacao.js
+++ js/validacao.js
@@ -1,5 +1,5 @@
 /*função valida campo*/
-function validar() {
+function enviar() {
 
     let name = formulario.name.value;
     let lcel = formulario.lcel.value;
@@ -7,72 +7,32 @@ function validar() {
     let lassunto = formulario.lassunto.value;
     let lmensagem = formulario.lmensagem.value;
 
-
-
-    /*Confere se os campos do formulario está preenchido*/
-    if (name == "") {
-
-        document.getElementById("resultado-name").innerHTML = "O Campo é obrigatório!";
-        document.getElementById('resultado-name').style.color = '#C6001A';
-        document.getElementById("name").style.borderColor = "#C6001A";
-        formulario.name.focus();
-    } else {
-        document.getElementById("name").style.borderColor = "black";
-        document.getElementById('resultado-name').innerHTML = ""
-    }
-    if (lcel == "") {
-        document.getElementById('resultado-lcel').innerHTML = "O Campo é obrigatório!";
-        document.getElementById('resultado-lcel').style.color = '#C6001A';
-        document.getElementById("lcel").style.borderColor = "#C6001A";
-        formulario.lcel.focus();
-    } else {
-        document.getElementById("lcel").style.borderColor = "black";
-        document.getElementById('resultado-lcel').innerHTML = ""
-    }
-
-    if (lemail == "") {
-        document.getElementById('resultado-email').innerHTML = "O Campo é obrigatório!";
-        document.getElementById('resultado-email').style.color = '#C6001A';
-        document.getElementById("lemail").style.borderColor = "#C6001A";
-        formulario.lemail.focus();
-    }
-    else {
-        document.getElementById('resultado-email').innerHTML = ""
-        document.getElementById("lemail").style.borderColor = "black";
-    }
-    if (lassunto == "") {
-        document.getElementById('resultado-assunto').innerHTML = "O Campo é obrigatório!"
-        document.getElementById('resultado-assunto').style.color = '#C6001A';
-        document.getElementById("lassunto").style.borderColor = "#C6001A";
-        formulario.lassunto.focus();
-    }
-
-    else {
-        document.getElementById('resultado-assunto').innerHTML = ""
-        document.getElementById("lassunto").style.borderColor = "black";
-    }
-    if (lmensagem == "") {
-        document.getElementById('resultado-mensagem').innerHTML = "O Campo é obrigatório!";
-        document.getElementById('resultado-mensagem').style.color = '#C6001A';
-        document.getElementById("lmensagem").style.borderColor = "#C6001A";
-        formulario.lmensagem.focus();
-    }
-    else {
-        document.getElementById('resultado-mensagem').innerHTML = ""
-        document.getElementById("lmensagem").style.borderColor = "black";
-    }
     if (lmensagem && lassunto && lcel && lemail && name != "") {
+
         formulario.reset();
-        confirm("Formulario enviado com sucesso!");
+        modal.style.display = "none";
+        document.getElementById('formularioexito').innerHTML = "Formulário enviado com Sucesso!";
+        document.getElementById('formularioexito').style.color = '#2A081A';
+        document.getElementById('formularioexito').style.font = 'italic';
     }
+}
+
+function cancelar() {
+
+    modal.style.display = "none";
+}
+
+function Limpar() {
+
+    document.getElementById("Limpar").reset();
 
 }
-function Limpar() {
-    document.getElementById("Limpar").reset();
-}
+
 function validacaoEmail(field) {
+
     usuario = field.value.substring(0, field.value.indexOf("@"));
     dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
+
     if ((usuario.length >= 1) &&
         (dominio.length >= 3) &&
         (usuario.search("@") == -1) &&
@@ -81,7 +41,7 @@ function validacaoEmail(field) {
         (dominio.search(" ") == -1) &&
         (dominio.search(".") != -1) &&
         (dominio.indexOf(".") >= 1) &&
-        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+        (dominio.lastIndexOf(".") <dominio.length - 1)) {
         document.getElementById("resultado-email").innerHTML = "";
     }
     else {
@@ -92,6 +52,7 @@ function validacaoEmail(field) {
     }
 
 }
+
 /* Mascara do celular*/
 const handlePhone = (event) => {
     let input = event.target
@@ -105,13 +66,13 @@ const phoneMask = (value) => {
     value = value.replace(/(\d)(\d{4})$/, "$1-$2")
     return value
 }
-var inputNome = document.querySelector("#nome");
+var inputNome = document.querySelector("#name");
 nome.addEventListener("keypress", function (e) {
     var keyCode = (e.keyCode ? e.keyCode : e.which);
 
     if (keyCode > 47 && keyCode < 58) {
         e.preventDefault();
-        
+
     }
 });
 
@@ -120,9 +81,9 @@ function lettersOnly(evt) {
     evt = (evt) ? evt : event;
     var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
         ((evt.which) ? evt.which : 0));
-    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
-        (charCode < 97 || charCode > 122)) {
-            document.getElementById("resultado-name").innerHTML= "<font color='red'>Não digite números nesse campo!</font>";
+    if (charCode >31 && (charCode <65 || charCode > 90) &&
+        (charCode <97 || charCode >122)) {
+        document.getElementById("resultado-name").innerHTML = "<font color='red'>Não digite números nesse campo!</font>";
         return false;
     }
     return true;
